a363c22d715b7d0dc4904c71348065aeb4f6c8f2
AVRaspopov <81385947+AVRaspopov@users.noreply.github.com>
2021-11-16 16:55:55 +0300

    Another way

--- src/main/java/ru/proektbg/assync/Main.java
+++ src/main/java/ru/proektbg/assync/Main.java
@@ -2,17 +2,23 @@ package ru.proektbg.assync;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         Test testThread = new Test();
         CompletableFuture<Long> result = CompletableFuture.supplyAsync(() -> testThread.calc());
-        while (result.isDone() != true){
-            System.out.println("Жду, пока выполнится таск...");
+        while (!result.isDone()){
             Thread.sleep(1000);
+            try {
+                System.out.println("Проверяю, выполнился ли таск");
+                System.out.println(result.get(0, TimeUnit.SECONDS));
+            } catch (TimeoutException e){
+                System.out.println("Таск еще не выполнился. Жду 1 секунду");
+            }
         }
-        System.out.println("Таск выполнился, ответ "+result.get());
     }
 
 }
